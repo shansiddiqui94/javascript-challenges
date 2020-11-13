41c738a4bb7a9b44babe05c44842d4c43a13b06a
shansiddiqui94 <49912126+shansiddiqui94@users.noreply.github.com>
2020-11-12 23:37:31 -0500

    Update balanced-parens.js

--- balanced-parens/balanced-parens.js
+++ balanced-parens/balanced-parens.js
@@ -1,5 +1,12 @@
-const parensAreBalanced = (input) => {
-  return false
-}
-
-module.exports = parensAreBalanced
+const parensAreBalanced = (str) => {
+    let inputSplt = input.split("");
+    return !inputSplt.reduce((prevChar, currChar) => {
+        if (currChar === "(" || currChar === "{" || currChar === "[") {
+            return ++prevChar;
+        } else if (currChar === ")" || currChar === "}" || currChar === "]") {
+            return --prevChar;
+        }
+        return prevChar;
+    }, 0);
+};
+module.exports = parensAreBalanced;
