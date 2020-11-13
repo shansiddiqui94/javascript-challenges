210f4cff623c37cc7b739648716d9bf2296e5f31
shansiddiqui94 <49912126+shansiddiqui94@users.noreply.github.com>
2020-11-12 23:36:42 -0500

    Update target-sum-exists.js

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -1,12 +1,27 @@
 const targetSumExistsInTwoNumbers = (list, target) => {
-  return false
-}
-
+    for (let i in list) {
+        for (let j in list) {
+            //see if list[i] + list[j] === target
+            if (list[i] + list[j] === target) {
+                return true;
+            }
+        }
+    }
+};
 const targetSumExistsInThreeNumbers = (list, target) => {
-  return false
-}
-
+    // return false;
+    for (let i in list) {
+        for (let j in list) {
+            //see if list[i] + list[j] === target
+            if (list[i] + list[j] === target) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+};
 module.exports = {
-  targetSumExistsInTwoNumbers,
-  targetSumExistsInThreeNumbers
-}
+    targetSumExistsInTwoNumbers,
+    targetSumExistsInThreeNumbers,
+};
