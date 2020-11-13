0b1a33ba55466ee2027e7171c1123b4294616d0f
shansiddiqui94 <49912126+shansiddiqui94@users.noreply.github.com>
2020-11-12 23:38:32 -0500

    Update add-eventing.js

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,18 @@
-const addEventing = function (obj) {
-}
-
-module.exports = addEventing
+const addEventing = (obj) => {
+    const eventings = [];
+    let objReturn = {
+        on: (event, callBack) => {
+            if (!eventings[event]) {
+                eventings[event] = [];
+            }
+            eventings[event].push(callBack);
+        },
+        trigger: (event, ...args) => {
+            if (eventings[event]) {
+                eventings[event].map((callback) => callback(...args));
+            }
+        },
+    };
+    return objReturn;
+};
+module.exports = addEventing;
